dbc944eb8b624f67f8064efaf7ba0e70a50f2a18
Jan Algermissen <algermissen@acm.org>
2021-01-17 01:24:24 +0100

    Add element

--- src/model/mod.rs
+++ src/model/mod.rs
@@ -3,3 +3,12 @@ pub struct Assembly {
     pub name: String,
     pub description: String,
 }
+
+///
+///
+#[derive(Serialize, Deserialize, Clone, Debug, Default)]
+pub struct Element {
+    pub id: String,
+    pub name: String,
+    pub description: String,
+}
