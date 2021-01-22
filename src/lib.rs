7064b842f5ebd907c1004bdd0e54b2ba0beb4199
Jan Algermissen <algermissen@acm.org>
2021-01-22 21:44:58 +0100

    0.1.1

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
 extern crate serde;
 #[macro_use]
 extern crate serde_derive;
-//#[macro_use]
 extern crate serde_json;
 
 pub mod model;
--- src/model/mod.rs
+++ src/model/mod.rs
@@ -1,14 +1,13 @@
-#[derive(Serialize, Deserialize, Clone, Debug, Default)]
-pub struct Assembly {
-    pub name: String,
-    pub description: String,
-}
-
-///
-///
+/// Implements the standardized payload format for
+/// ply elements.
 #[derive(Serialize, Deserialize, Clone, Debug, Default)]
 pub struct Element {
+    /// Globally unique ID of this element
     pub id: String,
+    /// Display name
     pub name: String,
+    /// Longer description of the meaning and significance
+    /// of this element.
+    #[serde(default)]
     pub description: String,
 }
