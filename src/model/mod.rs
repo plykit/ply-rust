63c6a1cc9e32ac7b8855ca7e7fc64dd539b021ea
Jan Algermissen <algermissen@acm.org>
2021-01-29 00:54:44 +0100

    Remove debug output

--- src/model/mod.rs
+++ src/model/mod.rs
@@ -26,7 +26,7 @@ impl std::str::FromStr for SolutionId {
     type Err = StringError;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        println!("parse Solution ID {}",s);
+        //println!("parse Solution ID {}",s);
         // TODO This is just dummy code to illustrate the validity check
         if s.len() > 10 {
             return Err(StringError{cause:"String to long".into()})
